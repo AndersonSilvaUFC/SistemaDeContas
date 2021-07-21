4c028c288396584798d0bb91fc50a5de8e0cdc81
AndersonSilvaUFC <andersonsilva@alu.ufc.br>
2021-07-21 18:39:44 -0300

    Histórico quase finalizado"

--- src/main/java/br/com/sitemadecontas/conta/ContaController.java
+++ src/main/java/br/com/sitemadecontas/conta/ContaController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -26,6 +27,65 @@ public class ContaController {
 	public ContaController(ContaRepository contaRepository, HttpSession session) {
 		this.contaRepository = contaRepository;
 		this.session = session;
+	}
+	
+	@GetMapping("/historico")
+	public ModelAndView historico() {
+		ModelAndView mv = new ModelAndView();
+		
+		if(session != null) {
+			Usuario usuario = (Usuario) session.getAttribute("usuarioLogado");
+			if(usuario != null) {
+				mv.setViewName("conta/historico");
+				List<Conta> contasPagas = contaRepository.findByUsuarioAndPaga(usuario, true);
+				mv.addObject("contasPagas", contasPagas);
+				
+				return mv;
+			}
+		}
+		
+		mv.setViewName("redirect:/login");
+		return mv;
+	}
+	
+	@GetMapping("/historico/excluir/{id}")
+	public ModelAndView excluirContaHistorico(@PathVariable int id){
+		ModelAndView mv = new ModelAndView();
+		
+		if(session != null) {
+			Usuario usuario = (Usuario) session.getAttribute("usuarioLogado");
+			if(usuario != null) {
+				Conta conta = contaRepository.findById(id);
+				if(conta != null && conta.isPaga() && conta.getUsuario().getId() == usuario.getId()) {
+					contaRepository.delete(conta);
+				}
+				
+				
+				return historico();
+			}
+		}
+		
+		mv.setViewName("redirect:/login");
+		return mv;
+	}
+	
+	@GetMapping("/historico/excluirHistorico")
+	public ModelAndView excluirHistorico() {
+		ModelAndView mv = new ModelAndView();
+		
+		if(session != null) {
+			Usuario usuario = (Usuario) session.getAttribute("usuarioLogado");
+			if(usuario != null) {
+				
+				contaRepository.deleteAllByUsuarioAndPaga(usuario, true);
+				
+				
+				return historico();
+			}
+		}
+		
+		mv.setViewName("redirect:/login");
+		return mv;
 	}
 	
 	@GetMapping("/home")
--- src/main/java/br/com/sitemadecontas/conta/ContaRepository.java
+++ src/main/java/br/com/sitemadecontas/conta/ContaRepository.java
@@ -2,7 +2,10 @@ package br.com.sitemadecontas.conta;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +15,11 @@ import br.com.sitemadecontas.usuario.Usuario;
 public interface ContaRepository extends JpaRepository<Conta, Integer>{
 	@Query("select c from Conta c where c.usuario = :usuario and c.paga = :paga")
 	List<Conta> findByUsuarioAndPaga(Usuario usuario, boolean paga);
+	
+	Conta findById(int id);
+	
+	@Transactional
+	@Modifying
+	@Query("delete from Conta c where c.usuario = :usuario and c.paga = :paga")
+	void deleteAllByUsuarioAndPaga(Usuario usuario, boolean paga);
 }
